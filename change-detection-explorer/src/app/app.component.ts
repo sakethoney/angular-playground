f116757c779e4b4b5dd851b9c6642adb7a453e13
sakethoney <saket.kumar.cs@gmail.com>
2024-10-02 00:54:06 -0400

    timer test

--- change-detection-explorer/src/app/app.component.ts
+++ change-detection-explorer/src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CounterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'change-detection-explorer';
+
+  get debugOutput() {
+    console.log('[AppComponent] "debugOutput" binding re-evaluated.');
+    return 'AppComponent Component Debug Output';
+  }
 }
---
+++ change-detection-explorer/src/app/counter/counter.component.ts
@@ -0,0 +1,34 @@
+import { Component, inject, NgZone, OnInit, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html',
+  styleUrl: './counter.component.css'
+})
+export class CounterComponent implements OnInit {
+
+  count = signal(0);
+
+  private zone = inject(NgZone);
+
+  get debugOutput() {
+    console.log('[Counter] "debugOutput" binding re-evaluated.');
+    return 'Counter Component Debug Output';
+  }
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.count.set(0);
+    }, 4000);
+
+    this.zone.runOutsideAngular(() => {
+      setTimeout(() => {
+        console.log('Timer expired from run outside angular');
+      }, 9000);
+    });
+
+  };
+
+}
